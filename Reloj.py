efaa6df3a5bd59c14189fa2bf20afafbb072e666
KathyCriollo <Kathy_eliza_1994@hotmail.com>
2016-06-22 21:19:33 -0500

    Reloj
    Programa terminado

---
+++ Reloj.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import time
+fecha_hora = time.strftime("%c")
+print ("Fecha y hora " + time.strftime("%c")) #Fecha y hora
+print ("Fecha "  + time.strftime("%x")) #tiempo
+print ("Hora " + time.strftime("%X")) #hora
+print ("Fecha y hora son: %s"  % fecha_hora ) 
